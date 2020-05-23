cc063190c64f2d1749ac38535ba200d3893e9216
aloyonnet <off.alexis.loyonnet@gmail.com>
2020-05-24 00:16:09 +0200

    feat: change login & security

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -1,53 +1,59 @@
 <?php
-
-namespace App\Controller;
-
-use App\Entity\User;
-use App\Form\RegistrationFormType;
-use App\Security\LoginFormAuthenticator;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
-
-class RegistrationController extends AbstractController
-{
-    /**
-     * @Route("/register", name="app_register")
-     */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
-    {
-        $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            // do anything else you need here, like send an email
-
-            return $guardHandler->authenticateUserAndHandleSuccess(
-                $user,
-                $request,
-                $authenticator,
-                'main' // firewall name in security.yaml
-            );
-        }
-
-        return $this->render('security/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
-    }
-}
+	
+	namespace App\Controller;
+	
+	use App\Entity\User;
+	use App\Form\RegistrationFormType;
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\Security\Core\Security;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Routing\Annotation\Route;
+	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+	use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+	
+	class RegistrationController extends AbstractController
+	{
+		
+		/**
+		 * @Route("/register", name="app_register")
+		 * @param Request $request
+		 * @param Security $security
+		 * @param UserPasswordEncoderInterface $passwordEncoder
+		 * @return Response
+		 */
+		public function register(Request $request, Security $security, UserPasswordEncoderInterface $passwordEncoder): Response
+		{
+			
+			if ($security->isGranted('ROLE_USER')) {
+				return $this->redirectToRoute('home');
+			}
+			
+			$user = new User();
+			$form = $this->createForm(RegistrationFormType::class, $user);
+			$form->handleRequest($request);
+			
+			if ($form->isSubmitted()) {
+				if($form->isValid()) {
+					$user->setPassword(
+						$passwordEncoder->encodePassword(
+							$user,
+							$form->get('plainPassword')->getData()
+						)
+					);
+					
+					$user->setUsername(preg_replace('/\s+/', '', $form->get('username')->getData()));
+					
+					$entityManager = $this->getDoctrine()->getManager();
+					$entityManager->persist($user);
+					$entityManager->flush();
+					
+					
+					return $this->redirectToRoute('app_login');
+				}
+			}
+			
+			return $this->render('security/register.html.twig', [
+				'registrationForm' => $form->createView(),
+			]);
+		}
+	}
