75a3e257051186bb3bf14dc3b249868f71299757
aloyonnet <off.alexis.loyonnet@gmail.com>
2020-05-23 20:16:09 +0200

    feat: add IsGranted for AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class AdminController
+ * @package App\Controller
+ * @IsGranted("ROLE_ADMIN")
+ */
 class AdminController extends AbstractController
 {
     /**
